19047ffb2389b340255fc0c5c7db1d96ba910d20
zeromountain <sonyeongsan@gmail.com>
2024-10-30 13:58:11 +0900

    feat: add post page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,21 +1,27 @@
 import bookmarkPlugin from '@notion-render/bookmark-plugin';
 import { NotionRenderer } from '@notion-render/client';
 import hljsPlugin from '@notion-render/hljs-plugin';
+
 import { BlockObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 import { BackButton } from '@/components/common/back';
-import { fetchBySlug, fetchPageBlocks, notion } from '@/lib/notion';
+import { fetchBySlug, fetchPageBlocks, fetchTags, notion } from '@/lib/notion';
 
 import BlogContent from './_components/blog-content';
 
 export default async function SlugPost({ params }: { params: { id: string } }) {
   const post = await fetchBySlug(params.id);
+  const tags = await fetchTags();
+
+  console.log({ tags });
 
   if (!post) {
     return <div>Not Found Post</div>;
   }
 
   const blocks = (await fetchPageBlocks(post.id)) as BlockObjectResponse[];
+
+  // console.log({ post });
 
   const renderer = new NotionRenderer({
     client: notion,
@@ -24,15 +30,14 @@ export default async function SlugPost({ params }: { params: { id: string } }) {
   renderer.use(hljsPlugin({}));
   renderer.use(bookmarkPlugin(undefined));
 
-  console.log({ block: blocks.find((block) => block.type === 'code')?.code.language });
-  const language = blocks.find((block) => block.type === 'code')?.code.language as string;
+  // console.log({ block: blocks.find((block) => block.type === 'code')?.code.language });
 
   const html = await renderer.render(...blocks);
 
   return (
     <>
       <BackButton />
-      {/* <BlogContent html={html} language={language} /> */}
+      {/* <BlogContent recordMap={post} /> */}
     </>
   );
 }
